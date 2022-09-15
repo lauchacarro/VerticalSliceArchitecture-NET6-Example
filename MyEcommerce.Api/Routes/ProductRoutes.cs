8e1acc713a1de7ce862ce70413e95685e571e27d
Lautaro Carro <lcarro@algeiba.com>
2022-09-14 21:19:51 -0300

    Add project files.

---
+++ MyEcommerce.Api/Routes/ProductRoutes.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+using MyEcommerce.Application.UseCases.Products.CreateProduct;
+
+namespace MyEcommerce.Api.Routes
+{
+
+    public static class ProductRoutes
+    {
+        const string PATH = "/Products";
+
+        public static IEndpointRouteBuilder MapProducts(this IEndpointRouteBuilder endpoint)
+        {
+            endpoint.MapPost(PATH, async (IMediator mediator, CreateProductRequest request)
+                => await mediator.Send(request).ToHttpResult());
+
+
+            //endpoint.MapPost(PATH, async (IMediator mediator, CreateProductRequest request) =>
+            //    {
+            //        return await mediator.Send(request).ToHttpResult();
+            //    });
+
+            return endpoint;
+        }
+    }
+
+}
