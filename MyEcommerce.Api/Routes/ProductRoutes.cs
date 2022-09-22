b5e6a63723be9b2bfc359b19438bf7f56d734aca
Lautaro Carro <lcarro@algeiba.com>
2022-09-22 10:22:33 -0300

    Agrego las funcionalidades de obtener todos los productos y producto por id

--- MyEcommerce.Api/Routes/ProductRoutes.cs
+++ MyEcommerce.Api/Routes/ProductRoutes.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 
 using MyEcommerce.Application.UseCases.Products.CreateProduct;
+using MyEcommerce.Application.UseCases.Products.GetProductById;
+using MyEcommerce.Application.UseCases.Products.GetProducts;
 
 namespace MyEcommerce.Api.Routes
 {
@@ -15,10 +17,16 @@ namespace MyEcommerce.Api.Routes
                 => await mediator.Send(request).ToHttpResult());
 
 
-            //endpoint.MapPost(PATH, async (IMediator mediator, CreateProductRequest request) =>
-            //    {
-            //        return await mediator.Send(request).ToHttpResult();
-            //    });
+            //Estos parametros se reciben desde QueryParams
+            endpoint.MapGet(PATH, async (string? search, int? take, int? skip, IMediator mediator)
+                => await mediator.Send(new GetProductsRequest(search, take, skip)).ToHttpResult());
+
+
+
+            //Este parametro se recibe desde la URL
+            endpoint.MapGet(PATH + "/{id}", async (int id, IMediator mediator)
+                => await mediator.Send(new GetProductByIdRequest(id)).ToHttpResult());
+
 
             return endpoint;
         }
